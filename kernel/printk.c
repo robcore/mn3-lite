0edb9e95f6bfdb1de027ef3579de5170b2965ea1
robcore <robpatershuk@gmail.com>
2021-08-31 11:05:27 -0600

    printk: Remove RTB logging

--- kernel/printk.c
+++ kernel/printk.c
@@ -49,7 +49,6 @@
 #include <linux/proc_fs.h>
 #endif
 
-#include <mach/msm_rtb.h>
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
 #ifdef LOCAL_CONFIG_PRINT_EXTRA_INFO
@@ -1068,11 +1067,6 @@ asmlinkage int printk(const char *fmt, ...)
 {
 	va_list args;
 	int r;
-#ifdef CONFIG_MSM_RTB
-	void *caller = __builtin_return_address(0);
-
-	uncached_logk_pc(LOGK_LOGBUF, caller, (void *)log_end);
-#endif
 
 #ifdef CONFIG_KGDB_KDB
 	if (unlikely(kdb_trap_printk)) {
